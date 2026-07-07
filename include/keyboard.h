c6350c05748e5a186e8489c2bf448f03f22e86ec
ArtorioXP <109192898+ArtorioXP@users.noreply.github.com>
2026-07-07 15:22:17 -0300

    feat: replace polling with interrupt-driven keyboard (IRQ1)

--- include/keyboard.h
+++ include/keyboard.h
@@ -1,6 +1,14 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
+#include <stdint.h>
+#include "isr.h"   // para registers_t
+
+void keyboard_init(void);
 char keyboard_read_char(void);
+uint32_t keyboard_get_irq_count(void);
+
+// Protótipo da função handler (agora não-static)
+void keyboard_irq_handler(registers_t* regs);
 
 #endif
