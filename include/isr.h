609d7fa9e2892c027724b59e2e1329004cb6c192
Joseph Antony <joseph.antony@dcomp.ufs.br>
2026-07-13 00:36:12 -0300

    feat: add uptime and IRQ diagnostic commands

--- include/isr.h
+++ include/isr.h
@@ -3,21 +3,33 @@
 
 #include <stdint.h>
 
-// Estrutura dos registros salvos pelo stub assembly
+/*
+ * Registros salvos pelos stubs Assembly.
+ */
 typedef struct {
-    uint32_t gs, fs, es, ds;  // Segmentos
-    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax; // GP registers
-    uint32_t int_no, err_code; // Número da int e erro
-    uint32_t eip, cs, eflags, user_esp, ss; // CPU push
+    uint32_t gs, fs, es, ds;
+    uint32_t edi, esi, ebp, esp;
+    uint32_t ebx, edx, ecx, eax;
+    uint32_t int_no, err_code;
+    uint32_t eip, cs, eflags, user_esp, ss;
 } registers_t;
 
-// Handlers
+/*
+ * Handlers principais.
+ */
 void isr_handler(registers_t regs);
-void irq_handler(registers_t* regs); 
+void irq_handler(registers_t* regs);
 
-// Registra callbacks para IRQs
+/*
+ * Registro de callbacks das IRQs.
+ */
 typedef void (*irq_handler_t)(registers_t* regs);
+
 void irq_register_handler(uint8_t irq, irq_handler_t handler);
 
+/*
+ * Retorna quantas vezes uma IRQ foi recebida.
+ */
+uint32_t irq_get_count(uint8_t irq);
 
 #endif
